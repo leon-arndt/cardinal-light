0bbe2d9e4458ae275bc69eeca80ea7267bb64d96
Frezer <Eike.S.Kreisel@hotmail.de>
2019-07-22 23:56:22 +0200

    + Added new placholder assets, + Added new Dev Scene (Eike), + Added dynamic environment shader, + Added Polybrush asset, + Pushed HDRP again (Package lost due to pulling

---
+++ Assets/Environment/LevelState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  gameObject.GetComponent<Renderer>().material.SetVector("_ObjectSource", new Vector3(500,0,500));
+        
+    }
+
+    bool envHealthy = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Renderer>().material.SetVector("_ObjectSource", new Vector4(500,500,500));
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            if (envHealthy)
+            {
+                Shader.SetGlobalFloat("_levelstate", 0);
+                envHealthy = false;
+            }
+            else
+            {
+                Shader.SetGlobalFloat("_levelstate", 1);
+                envHealthy = true;
+            }
+
+        }
+    }
+}
